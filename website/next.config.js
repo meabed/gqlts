7a506cd9d7f6f2882c8481da435161bf8b2e92b6
Mohamed Meabed <mo.meabed@gmail.com>
2022-05-11 02:21:55 -0400

    chore: update nextjs

--- website/next.config.js
+++ website/next.config.js
@@ -2,4 +2,4 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
 });
-module.exports = withNextra({});
+module.exports = withNextra({ dev: process.env.NODE_ENV !== "production" });
