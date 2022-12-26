e86414ad54388f2c54065cdef506384cf819c2cd
Mohamed Meabed <mo.meabed@gmail.com>
2022-12-25 23:40:19 -0500

    chore: update release

--- release.config.js
+++ release.config.js
@@ -1,43 +1,45 @@
 // https://semantic-release.gitbook.io/semantic-release/usage/configuration
-const pkg = require('./package.json');
-const branch = process.env.BRANCH || process.env.CI_REF_NAME_SLUG || '';
-const isMaster = branch === 'master' || branch === 'main';
+const pkg = require("./package.json");
+const branch = process.env.BRANCH || process.env.CI_REF_NAME_SLUG || "";
+const isMaster = branch === "master" || branch === "main";
 // semantic-release configuration
 module.exports = {
   branches: [
     {
-      name: 'master',
+      name: "master",
       prerelease: false,
     },
     { name: branch, prerelease: true },
   ],
   plugins: [
     [
-      '@semantic-release/commit-analyzer',
+      "@semantic-release/commit-analyzer",
       {
-        preset: 'angular',
+        preset: "angular",
         releaseRules: [
-          { type: 'breaking', release: 'major' },
-          { type: 'feat', release: 'minor' },
-          { type: 'fix', release: 'patch' },
-          { type: 'revert', release: 'patch' },
-          { type: 'docs', release: 'patch' },
-          { type: 'refactor', release: 'patch' },
-          { type: 'style', release: 'patch' },
-          { type: 'test', release: 'patch' },
-          { type: 'chore', release: 'patch' },
-          { type: 'ci', release: 'patch' },
-          { type: 'perf', release: 'patch' },
-          { type: 'build', release: 'patch' },
+          { type: "breaking", release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "revert", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "test", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "build", release: "patch" },
         ],
       },
     ],
-    ['@semantic-release/release-notes-generator'],
+    ["@semantic-release/changelog"],
+    ["@semantic-release/release-notes-generator"],
+    ["semantic-release-lerna", { generateNotes: true }],
     // https://github.com/semantic-release/npm
-    ['@semantic-release/npm'],
+    ["@semantic-release/npm"],
     // https://github.com/semantic-release/github
     [
-      '@semantic-release/github',
+      "@semantic-release/github",
       {
         successComment: false,
         failComment: false,
@@ -45,10 +47,10 @@ module.exports = {
     ],
     // https://github.com/semantic-release/git
     isMaster && [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        assets: ['package.json', 'package-lock.json', 'yarn.lock', 'npm-shrinkwrap.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        assets: ["package.json", "package-lock.json", "yarn.lock", "npm-shrinkwrap.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
         GIT_AUTHOR_NAME: pkg.author.name,
         GIT_AUTHOR_EMAIL: pkg.author.email,
         GIT_COMMITTER_NAME: pkg.author.name,
