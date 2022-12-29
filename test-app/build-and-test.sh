9502462172d46d7f692eed28a864c0807bfac088
Mohamed Meabed <mo.meabed@gmail.com>
2022-12-29 12:45:38 -0500

    chore: update test

--- test-app/build-and-test.sh
+++ test-app/build-and-test.sh
@@ -4,8 +4,8 @@ my_dir="$(dirname "$0")"
 cd $my_dir/backend
 yarn install --frozen-lockfile
 yarn dev --exit-after-generate-schema
+yarn build-sdk
 yarn typecheck
 yarn build
 yarn start --exit-after-generate-schema
-yarn build-sdk
 yarn test
