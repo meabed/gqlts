21e59a7f5f3d0ceeaf87a48fabf5f0c8ed56d2f0
Mohamed Meabed <mo.meabed@gmail.com>
2022-12-30 04:35:19 -0500

    chore: update test

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
-
+my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+preVersion=$(node -p "require('./package.json').version")
 pkgVersion=$(cat ./next-version.txt)
 branch=$1
 
@@ -13,9 +14,17 @@ echo "pkgVersion: $pkgVersion"
 npm version $pkgVersion --no-git-tag-version --allow-same-version --no-commit-hooks --workspace-update=false
 npx lerna version $pkgVersion --no-git-tag-version --no-push --yes
 
+# replace old versions in files
+find . -type f \( -name '*.js' -or -name '*.ts' -or -name '*.tsx' -or -name '*.json' \) -not -path '*node_modules*' -not -path '*.next*' -exec grep -l "$pkgVersion" {} \; | xargs perl -pi -e "s/$pkgVersion/$newVersion/g"
+
 yarn install --frozen-lockfile || true
 yarn install --frozen-lockfile
+## todo fix this by hoisting the repo https://github.com/graphql-compose/graphql-compose-examples/tree/master/examples/northwind
+# yarn examplecli
+#
 yarn buildall
+#
+yarn genall
 yarn test
 
 echo "Updating repo..."
@@ -29,12 +38,12 @@ if [[ $branch == "master" ]]; then
   echo "Repo pushed."
 fi
 
-cd ./runtime
+cd $my_dir/runtime
 runtimeVersion=$(node -p -e "require('./package.json').version")
 echo "runtimeVersion: $runtimeVersion"
 npm publish --access public
 
-cd ../cli
+cd $my_dir/cli
 cliVersion=$(node -p -e "require('./package.json').version")
 echo "cliVersion: $cliVersion"
 npm publish --access public
--- try-clients/hasura/generated/index.js
+++ try-clients/hasura/generated/index.js
@@ -6,7 +6,7 @@ const {
 } = require('@gqlts/runtime')
 var typeMap = linkTypeMap(require('./types.cjs'))
 
-var version = '3.2.10'
+var version = '3.2.11'
 assertSameVersion(version)
 
 module.exports.version = version
