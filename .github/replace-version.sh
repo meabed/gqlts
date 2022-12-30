e92990ba91288a6640578675497772b99cb111c6
Mohamed Meabed <mo.meabed@gmail.com>
2022-12-30 16:18:15 -0500

    chore: update pkgs

--- .github/replace-version.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pkgVersion="3.2.10"
-newVersion="3.2.11"
-find . -type f \( -name '*.js' -or -name '*.ts' -or -name '*.tsx' -or -name '*.json' \) -not -path '*node_modules*' -not -path '*.next*' -exec grep -l "$pkgVersion" {} \; | xargs perl -pi -e "s/$pkgVersion/$newVersion/g"
--- publish.sh
+++ publish.sh
@@ -2,21 +2,22 @@
 
 set -e
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-preVersion=$(node -p "require('./package.json').version")
+oldVersion=$(node -p "require('./package.json').version")
 pkgVersion=$(cat ./next-version.txt)
 branch=$1
 
 npm config set '//registry.npmjs.org/:_authToken' "$NPM_TOKEN"
 
+echo "Old version: $oldVersion"
 echo "Publishing version $pkgVersion - $branch"
 
 echo "pkgVersion: $pkgVersion"
 npm version $pkgVersion --no-git-tag-version --allow-same-version --no-commit-hooks --workspace-update=false
 npx lerna version $pkgVersion --no-git-tag-version --no-push --yes
-
+echo "Package version updated to $pkgVersion"
+echo "-----------------------------------"
 # replace old versions in files
-find . -type f \( -name '*.js' -or -name '*.ts' -or -name '*.tsx' -or -name '*.json' \) -not -path '*node_modules*' -not -path '*.next*' -exec grep -l "$pkgVersion" {} \; | xargs perl -pi -e "s/$pkgVersion/$newVersion/g"
-
+find . -type f \( -name '*.js' -or -name '*.ts' -or -name '*.tsx' -or -name '*.json' \) -not -path '*node_modules*' -not -path '*.next*' -exec grep -l "$pkgVersion" {} \; | xargs perl -pi -e "s/$oldVersion/$pkgVersion/g"
 yarn install --frozen-lockfile --ignore-scripts
 ## todo fix this by hoisting the repo https://github.com/graphql-compose/graphql-compose-examples/tree/master/examples/northwind
 # yarn examplecli
