243f07c01b3530f1bc7a9fd9f65ed41fce85d6d6
Mohamed Meabed <mo.meabed@gmail.com>
2024-10-28 15:17:51 -0400

    chore: update

--- demo-apps/build-and-test.sh
+++ demo-apps/build-and-test.sh
@@ -3,7 +3,7 @@ set -e
 my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $my_dir/backend
-yarn install --frozen-lockfile
+yarn install --frozen-lockfile --check-files
 yarn dev --exit-after-generate-schema
 yarn build-sdk
 yarn typecheck
@@ -12,21 +12,24 @@ yarn start --exit-after-generate-schema
 yarn test
 
 # test the standalone bundle
+echo "Testing standalone bundle"
 cd $my_dir/html
-yarn install --frozen-lockfile
+yarn install --frozen-lockfile --check-files
 npx --yes concurrently --kill-others --success first --names "backend,frontend" \
   "cd $my_dir/backend && yarn start" \
   "cd $my_dir/html && yarn test"
 
 # test react nextjs app with sdk ( ssr / csr / api routes )
+echo "Testing react nextjs app"
 cd $my_dir/next
-yarn install --frozen-lockfile
+yarn install --frozen-lockfile --check-files
 npx --yes concurrently --no-install --kill-others --success first --names "backend,frontend" \
   "cd $my_dir/backend && yarn start" \
   "cd $my_dir/next && yarn dev" \
   "cd $my_dir/next && yarn test"
 
 # test with production build
+echo "Testing production build"
 cd $my_dir/next
 yarn build
 npx --yes concurrently --no-install --kill-others --success first --names "backend,frontend" \
